969cd3a745f57391c177a25518458175a61a4483
Mike <maltidor86@gmail.com>
2025-03-19 13:31:30 -0400

    added Adnan lastname

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "The DevOps journey is amazing and I'm having fun learning this with Adnan"
+    return "The DevOps journey is amazing and I'm having fun learning this with Adnan Fakhar"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
