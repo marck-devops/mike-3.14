e7ca9ba6f4617fd66c3e0f7a0ae519ac7715fd63
marck-devops <marck.devops@gmail.com>
2025-06-13 12:58:22 -0400

    Terraforms Completed

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "This DevOps journey is amazing and I'm having fun learning this with Adnan Fakhar (AWS)"
+    return "This DevOps journey is amazing and I'm having fun learning this with Adnan Fakhar. We just finished learning Terraforms and it blew my mind; it was really fun. (AWS)"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
